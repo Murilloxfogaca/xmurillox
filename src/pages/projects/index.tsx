254162596f25d6506106da88e9648249f8f8ddfa
Murilloxfogaca <sxecesar@gmail.com>
2022-12-04 00:33:48 -0300

    :sparkles: new layout

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -2,6 +2,10 @@ import Head from "next/head";
 import styles from "../../shared/styles/Projects.module.css";
 import Ref from "../../shared/components/projects/Card";
 import Link from "next/link";
+import Makeitstop from "../../assets/uploads/makeitstop.jpg";
+import Rubrica from "../../assets/uploads/rubrica.jpg";
+import Salaodolivro from "../../assets/uploads/salaodolivro.jpg";
+import Terraviva from "../../assets/uploads/terraviva.jpg";
 
 export default function Projects() {
   return (
@@ -13,39 +17,34 @@ export default function Projects() {
       </Head>
 
       <main className={styles.main}>
-        <div className="container py-5">
-          <div className={styles.row}>
             <Ref
+              img={Makeitstop.src}
               url="https://insidea5.com.br/"
               title="Inside A5"
               subtitle="Blog Punk"
               text="Producer of events in the interior of the State of São Paulo, since 2014 producing shows, together with the digital format, creates content for the punk movement in the region of SP"
-            />
+              />
             <Ref
-              url="https://orubrica.com.br/"
+              img={Rubrica.src}
+              url="#"
               title="Rubrica"
               subtitle="Graphic Design and Workshops"
               text="Rubrica is an initiative by Felipe Fogaça that started in 2018 with the idea of ​​expanding some subversive graphic works linked to music and politics."
-            />
+              />
             <Ref
+              img={Salaodolivro.src}
               url="https://salaodolivropolitico.com.br/"
               title="Salão do livro político"
               subtitle="Book fair"
               text="Book Fair from September 24th to October 3rd in partnership with more than 50 Independent Publishers, on the youtube channel of TV Boitempo and TV PUC SP"
-            />
+              />
             <Ref
+              img={Terraviva.src}
               url="#"
               title="Terra Viva Armazém"
               subtitle="Manager system to aliments"
               text="Organic garden management system for small farmers"
             />
-          </div>
-          <p className="text-center py-4 text-uppercase font-bold">
-            <Link href="/">
-              <a>Back</a>
-            </Link>
-          </p>
-        </div>
       </main>
     </div>
   );
--- src/shared/components/projects/Card/index.tsx
+++ src/shared/components/projects/Card/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from "../../../styles/Projects.module.css";
 
 function Card(props) {
     return (
-        <div className="px-3">
-            <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-4">
+        <div className={styles.paralax} style={{backgroundImage: `url(${props.img})`}}>
+            <div className="max-w-lg text-center py-4">
                 <div>
-                    <h2 className="text-gray-800 text-3xl font-semibold">{props.title}</h2>
-                    <h3 className="text-gray-800 text-2xl font-semibold">{props.subtitle}</h3>
-                    <p className="mt-2 text-gray-600">{props.text}</p>
+                    <h2 className="text-white text-7xl font-bold">{props.title}</h2>
+                    <h3 className="text-white text-3xl font-semibold mb-4">{props.subtitle}</h3>
+                    <p className="mt-2 text-white">{props.text}</p>
                 </div>
-                <div className="flex justify-end mt-4">
-                    <Link href={props.url} >
-                        <a target="_blank"  rel="noreferrer"  className="text-xl font-medium btn btn-blue">See more</a>
-                    </Link>
+                <div className="flex justify-center mt-4">
+                    {props.url === "#" ? (
+                        <a target="_blank"  rel="noreferrer"  className="text-xl font-medium btn btn-blue">Coming soon</a>
+                    ) : (
+                        <Link href={props.url} >
+                            <a target="_blank"  rel="noreferrer"  className={styles.btn}>See more</a>
+                        </Link>
+
+                    )}
                 </div>
             </div>
         </div>
